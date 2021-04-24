837c2491e5f72389db0054a107a156b4c0bcb8ec
Artiom P <apoluyanovich@gmail.com>
2021-04-24 14:03:51 +0300

    Project creation

---
+++ BestPlaces/BestPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BestPlaces
+//
+//  Created by Artiom Poluyanovich on 24.04.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
